44bacffb466c6112bb929804f3bf4f03fd944b9f
Gintaras <garstikaitis@gmail.com>
2023-01-28 22:11:53 +0100

    docs(web.php): change json response to include new route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
-	return response()->json(['success' => true, 'data' => null, 'message' => 'visit /api for some magic']);
+Route::get("/", function () {
+    return response()->json([
+        "success" => true,
+        "data" => null,
+        "message" => "visit /api/push-forecast for some magic",
+    ]);
 });
